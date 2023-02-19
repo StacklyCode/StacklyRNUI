9d739a56c92839b1dd8a06a1794cd9e3b7d6ae2c
Yuborama <davidjohan2@hotmail.com>
2023-02-19 02:47:03 -0500

    feat: export button and modal

--- core/@atoms/index.ts
+++ core/@atoms/index.ts
@@ -2,3 +2,5 @@ export { default as AtomText } from "./AtomText";
 export { default as AtomWrapper } from "./AtomWrapper";
 export { default as AtomIcon } from "./AtomIcon";
 export { default as AtomImage } from "./AtomImage";
+export { default as AtomButton } from "./AtomButton";
+export { default as AtomModal } from "./AtomModal";
